d0cecdf934dab77bcf8b043f9579ab8e669567c3
CptSchmetterling <214170343+CptSchmetterling@users.noreply.github.com>
2026-06-03 19:41:40 +0200

    Menü. Spielschleife und Auswahl in main.py

--- main.py
+++ main.py
@@ -1,109 +1,75 @@
-class Monster:
-    def __init__(self, name: str, hp: int,defense: int, attack: int,typ:str):
-        self.name = name
-        self.hp = hp
-        self.max_hp = hp
-        self.defense = defense
-        self.attack = attack
-        self.type = typ
-
-    def __str__(self):
-        return (f"{self.name}({self.hp}/{self.max_hp} KP)"
-        )
+import random
+from buttonpress import text_and_wait_for_enter, auswahl_loop
+from gamestate import GameState
+from monsters import get_monsterlist
+from attack import do_attack
 
 
-class Attack:
-    def __init__(self, name: str, power: int, typ: str):
-        self.name = name
-        self.power = power
-        self.typ = typ.lower()
 
-    def typenvorteil(self,target_monster_type: str)-> float:
-        target_type = target_monster_type.lower()
-        multiplier = 1.0
-        wechselwirkung = {
-            "feuer": {"strong": "pflanze", "weak": "wasser" },
-            "wasser": {"strong": "feuer", "weak": "pflanze"},
-            "pflanze": {"strong": "wasser", "weak": "feuer"},}
+def menu():
+    text_and_wait_for_enter(
+        "======================\n"
+        "\tMONSTERBATTLER\n\n"
+        "   Adventure Awaits\n"
+        "======================")
 
-        if self.typ in wechselwirkung:
-            if wechselwirkung[self.typ]["strong"] == target_type:
-                multiplier = 2.0
-                print("Es ist sehr effektiv!")
-            elif wechselwirkung[self.typ]["weak"] == target_type:
-                multiplier = 0.5
-                print("Es ist nicht sehr effektiv...")
-        return multiplier
 
-#Monster
-bisasam = Monster("Bisasam", 45, 49, 49,"Pflanze")
-glumanda = Monster("Glumanda", 39, 52, 43,"Feuer")
-schiggy = Monster("Schiggy", 44, 65, 49, "Wasser")
-#Attacken
-tackle = Attack("Tackle", 40, "Normal")
-watergun = Attack("Water Gun", 40, "Wasser")
-ember = Attack("Ember", 40, "Feuer")
-vinewhip = Attack("Vine Whip", 40, "Pflanze")
+def auswahl_monster(monsterlist: list, player_name: str):
+    print(f"\n {player_name}, wähle dein Monster:")
+    for i, m in enumerate(monsterlist, start = 1):
+        print(f"[{i}]. {m.name} (Typ: {m.type.capitalize()})")
 
-"""  
-class AttackSet:
-    pro pokemon
-"""""
+    return auswahl_loop(monsterlist)
 
-class GameState:
-    def __init__(self,monster_player: Monster, monster_cpu: Monster):
-        self.monster_player = monster_player
-        self.monster_cpu = monster_cpu
-        self.active_player = "Player"
 
-    def switch_turn(self):
-        if self.active_player == "Player":
-            self.active_player = "Cpu"
+def auswahl_attack(monster, is_cpu: bool = False):
+    if is_cpu:
+        attack = random.choice(monster.attacks)
+        print(f"\nCPU wählt: {attack.name}")
+        return attack
+
+    print(f"\n {monster.name}, - wähle eine Attacke:")
+    for i, atk in enumerate(monster.attacks, start=1):
+        print(f"[{i}]. {atk.name} (Typ: {atk.type.capitalize()}, Power:{atk.power})")
+
+    return auswahl_loop(monster.attacks)
+
+
+def battle_loop(game: GameState):
+    """Main battle loop"""
+    while game.player_monster.is_alive() and game.cpu_monster.is_alive():
+        print(game)
+
+        active = game.get_active_monster()
+
+        if game.active_player == "Player":
+            attack = auswahl_attack(active)
         else:
-            self.active_player = "Player"
-        print(f"{game.get_active_monster()} ist am Zug!\n")
+            attack = auswahl_attack(active, is_cpu=True)
 
-#gibt Aktives Monster zurück
-    def get_active_monster(self)-> Monster:
-        if self.active_player == "Player":
-            return self.monster_player
-        return self.monster_cpu
+        do_attack(attack, game)
 
-#Monster das angegriffen wird
-    def get_attacked_monster(self)-> Monster:
-        if self.active_player == "Player":
-            return self.monster_cpu
-        return self.monster_player
-
-    def do_attack(self, attack: Attack):    #noch direkt Attack
-        attacker = self.get_active_monster()
-        defender = self.get_attacked_monster()
-
-        print(f"{attacker} setzt {attack.name} ein!\n")
-
-        multiplier = attack.typenvorteil(defender.type)
-
-        damage = int(((2.4 * (attack.power * attacker.attack / defender.defense) / 50 )+2) * multiplier)
-        """Schadensformel: 2⋅Level5+2⋅Stärke der Attacke⋅AngriffVerteidigung÷50 * multiplier"""
-
-        defender.hp -= damage
-        if defender.hp < 0:
-            defender.hp = 0
-        print(f"{defender.name} erleidet {damage} Schaden!\n")
-        game.switch_turn()
+    print("\n===========ENDE===========")
+    if game.player_monster.is_alive():
+        print(f"{game.player_monster.name} WINS")
+    else:
+        print(f"{game.cpu_monster.name} war stärker.\nGood luck next time!")
 
 
-    def __str__(self):
-        return (f"-----------------------------------\n"
-                f"Spieler: {self.monster_player} "
-                f"CPU: {self.monster_cpu} "
-                f"> > > {self.active_player.upper()}'s turn\n")
+def main():
+    # Startet Menü
+    menu()
+    # lädt Monsterkiste
+    monsterlist = get_monsterlist()
+    # Monsterauswahl
+    players_monster = auswahl_monster(monsterlist, "Player")
+    # Auswahl zufälliges CPU
+    cpus_monster = random.choice([m for m in monsterlist])
+    print(f"\nCPU wählt: {cpus_monster.name}!\n")
+
+    game = GameState(players_monster, cpus_monster)
+    battle_loop(game)
 
 
 if __name__ == "__main__":
-    game = GameState(bisasam, schiggy)  ### placeholder hardcoded monster
-    print(game)
-    game.do_attack(vinewhip)
-    print(game)
-    game.do_attack(tackle)
-    print(game)
+    main()
